90458114818e28f32f73f441bf2af9c2a8a36f86
qw0 <hello@quarterwave.io>
2024-03-09 19:01:39 -0800

    Added finite differences

---
+++ src/finitedifference.rs
@@ -0,0 +1,54 @@
+
+pub fn df1(eval_function: &dyn Fn(&f32) -> f32, x: f32, h: f32, mode: i8) -> f32 {
+    match mode{
+        0=> return (eval_function(&(x+h)) - eval_function(&(x)))/h, //forward
+        1=> return (eval_function(&(x)) - eval_function(&(x-h)))/h, //backward
+        2=> return (eval_function(&(x+(h/2.0))) - eval_function(&(x-(h/2.0))))/h, //central
+        _=> return (eval_function(&(x+(h/2.0))) - eval_function(&(x-(h/2.0))))/h //central again
+    }
+}
+
+pub fn df2(eval_function: &dyn Fn(&f32, &f32) -> f32, x: f32, y: f32, h: f32, k: f32, mode: i8) -> [f32; 2] { 
+
+    let partial_x = |xp: &f32| {eval_function(&xp, &y)}; //∂f/dx
+    let partial_y = |yp: &f32| {eval_function(&x, &yp)}; //∂f/dy
+
+    let pder_x: f32 = df1(&partial_x, x, h, mode);
+    let pder_y: f32 = df1(&partial_y, y, k, mode);
+
+    let derivative: [f32; 2] = [pder_x, pder_y];
+
+    derivative
+}
+
+pub fn differentiate_1d(eval_function: &dyn Fn(&f32) -> f32, lower_bound:f32, upper_bound:f32, h:f32, mode: i8) -> Vec<f32> {
+    let point_number: i32 = ((upper_bound - lower_bound) / h).round() as i32;
+    
+    let mut derivative: Vec<f32> = vec![0.0; point_number as usize];
+
+    for i in 0..point_number {
+        let x: f32 = lower_bound + ((i as f32) * h);
+        derivative[i as usize] = df1(eval_function, x, h, mode);
+    }
+
+    derivative
+}
+
+pub fn differentiate_2d(eval_function: &dyn Fn(&f32, &f32) -> f32, lower_bound_x:f32, upper_bound_x:f32, lower_bound_y:f32, upper_bound_y:f32, h: f32, k: f32, mode: i8) -> Vec<Vec<[f32; 2]>> {
+
+    let point_number_x: i32 = ((upper_bound_x - lower_bound_x) / h).round() as i32;
+    let point_number_y: i32 = ((upper_bound_y - lower_bound_y) / k).round() as i32;
+
+
+    let mut gradient: Vec<Vec<[f32; 2]>> = vec![vec![[0.0; 2]; point_number_y as usize]; point_number_x as usize]; // [x coordinate][y coordinate][(∂f/dx, ∂f/dy)]
+
+    for i in 0..point_number_x{
+        for j in 0..point_number_y{
+            let x: f32 = lower_bound_x + ((i as f32) * h);
+            let y: f32 = lower_bound_y + ((j as f32) * k);
+            gradient[i as usize][j as usize] = df2(&eval_function, x, y, h, k, mode);
+        }
+    }
+
+    gradient
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod trapezoidal;
+mod finitedifference;
 
 fn main() {
 
     let integral_soltuion_1d: f32 = trapezoidal::trapz(&fint, &-1.0, &1.0, &0.01, false);
     let integral_solution_2d: f32 = trapezoidal::trapz_2(&fint2, &-4.0, &-4.0, &4.0, &4.0, &0.01, &0.01);
+    let differentiated_1d: Vec<f32> = finitedifference::differentiate_1d(&fint, -1.0, 1.0, 0.001, 3);
+    let differentiated_2d: Vec<Vec<[f32; 2]>> = finitedifference::differentiate_2d(&fint2, -2.0, 2.0, -2.0, 2.0, 0.001, 0.001, 3);
 
-    println!("Evaluated 1D: {0}", integral_soltuion_1d);
-    println!("Evaluated 2D: {0}", integral_solution_2d);
+    println!("Integral 1D: {0}", integral_soltuion_1d);
+    println!("Integral 2D: {0}", integral_solution_2d);
+
+    println!("Derivative 1D: {0}", differentiated_1d[((2.0/0.001)-1.0) as usize]);
+    println!("Derivative 2D: [{0}, {1}]", differentiated_2d[3999][3999][0], differentiated_2d[3999][3999][1]);
 }
 
 fn fint(x: &f32) -> f32 {
@@ -16,4 +22,3 @@ fn fint(x: &f32) -> f32 {
 fn fint2(x: &f32, y: &f32) -> f32 {
     (x.powf(2.0))*(y.powf(2.0))
 }
-
